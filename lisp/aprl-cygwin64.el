435c08a8d26b6e495a672987ef6a3b5f69fe6b72
stakahama <stakahama@APRLPCX.(none)>
2013-12-18 10:05:47 +0100

    w32-short-file-name definition fset to identity

--- lisp/aprl-cygwin64.el
+++ lisp/aprl-cygwin64.el
@@ -1,13 +1,11 @@
-;;http://stackoverflow.com/questions/9167614/python-mode-in-emacs-no-such-file-or-directory-pdb
+;; required for finding cygwin executables
+;; http://stackoverflow.com/questions/9167614/python-mode-in-emacs-no-such-file-or-directory-pdb
 (when (eq system-type 'cygwin)
   (setenv "PATH" (concat "/usr/bin:" (getenv "PATH")))
   (setq exec-path (split-string (getenv "PATH") path-separator)))
 
-;;https://stat.ethz.ch/pipermail/ess-help/2004-August/002010.html
-(when (or (eq system-type 'cygwin)
-	  (eq system-type 'windows-nt))
-  (defun w32-short-file-name (x)
-    "Placeholder for use with Xemacs.
-The real function in Emacs returns the short file name version (8.3) of
-the full path of FILENAME. This placeholder version returns its argument."
-    x))
+;; required by ESS
+(when (and (or (eq system-type 'cygwin)
+	       (eq system-type 'windows-nt))
+	   (not (fboundp 'w32-short-file-name)))
+  (fset 'w32-short-file-name 'identity))
