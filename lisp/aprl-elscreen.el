e422b85c43f4faea36609ed541d901e957d70490
Satoshi Takahama <satoshi.takahama@gmail.com>
2014-08-23 20:36:43 +0200

    edited aprl-elscreen.el

--- lisp/aprl-elscreen.el
+++ lisp/aprl-elscreen.el
@@ -1,26 +1,15 @@
 ;; installed from https://github.com/shosti/elscreen
 ;;   not in MELPA repo as advertised
 
-;;;_* ===== Elscreen ====
-
-;;;_ --- load ---
+;;;_* --- load ---
 (add-to-list 'load-path (aprl-search-package 'elscreen "~/.emacs.d/site-lisp"))
 (load "elscreen" "ElScreen" t)
 (elscreen-start) ;new requirement
 
-;;;_ --- key bindings ---
-(global-set-key (kbd "<f6>"  ) 'elscreen-reset)
-(global-set-key (kbd "<f7>") 'elscreen-next)
-(global-set-key (kbd "S-<f7>") 'elscreen-previous)
-(global-set-key (kbd "<f8>"    ) 'elscreen-create)
-(global-set-key (kbd "S-<f8>"  ) 'elscreen-kill)
-(define-key elscreen-map "f" 'elscreen-find-file)
+;;;_* --- key bindings ---
 (define-key elscreen-map "r" 'elscreen-reset)
-(define-key elscreen-map "l" 'elscreen-create)
-(global-set-key (kbd "S-C-<left>") 'elscreen-previous)
-(global-set-key (kbd "S-C-<right>") 'elscreen-right)
 
-;;;_ ... functions ---
+;;;_* ... functions ---
 (defun elscreen-reset ()
   "Cycles through screens so that window configurations are reset (prevents flashing from redraw-frame[?] after each keystroke)"
   (interactive)
